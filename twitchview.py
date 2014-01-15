42d01fc2fbcd4075b6b6251de548e2ccf5d465d0
Ac-town <actown@gmail.com>
2014-01-15 10:27:01 -0800

    First public version of twitchview.
    Twitchview as of right now will print out your first 100 followed
    channels and display if they are live. It will also show the title of
    the channel. To use: python twitchview.py -u username

---
+++ twitchview.py
@@ -0,0 +1,18 @@
+import urllib2
+import json
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-u", "--user", dest="username",
+                  help="Your twitch.tv username.")
+(options, args) = parser.parse_args()
+
+url = 'https://api.twitch.tv/kraken/users/%s/follows/channels?limit=100' % options.username
+follows = json.load(urllib2.urlopen(url))
+streamers = ''
+for val in follows['follows']:
+    streamers = streamers+val['channel']['name'] + ','
+url = 'https://api.twitch.tv/kraken/streams?channel=%s' % streamers
+live = json.load(urllib2.urlopen(url))
+for val in live['streams']:
+    print val['channel']['name'] + ' - ' + val['channel']['status']
